639261ee6add2171d3cd0f7310495c7a64c35034
Hoàng Minh Quân <quanhm@hachinet.com>
2023-12-13 10:22:34 +0000

    Update image/Asian Dolce Latte.jpg, image/Café Latte.jpg, image/Café Mocha.jpg, image/Caffè Americano.jpg, image/Flavored Latte.jpg, image/Hazelnut Macchiato.jpg, image/Ristretto Bianco.jpg, image/Skinny Flavored Latte.jpg, models/purchaseModel.cjs, node_modules/.package-lock.json

---
+++ models/purchaseModel.cjs
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Define the Purchase schema
+const purchaseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      productId: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  purchaseDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the Purchase model
+const Purchase = mongoose.model("Purchase", purchaseSchema);
+
+module.exports = Purchase;
